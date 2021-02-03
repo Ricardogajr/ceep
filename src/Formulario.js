64f502755591a65049bea4af3f638fc088ec19a7
= <=>
2021-02-02 21:20:40 -0300

    Teste

---
+++ src/Formulario.js
@@ -0,0 +1,9 @@
+function Formulario() {
+    return (<form>
+        <input type="text" placeholder="Titulo"></input>
+        <textarea name="" placeholder="Escreva sua nota."></textarea>
+        <button>Criar nota</button>
+    </form>);
+}
+
+export default Formulario;
